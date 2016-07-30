0b18eb38fce0668aedd61893bb83496c5854199b
Patrick B. Buehl <pbb91@yahoo.com>
2016-07-30 18:56:42 -0500

    Add required variables into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 /**
  * Created by Patrick on 7/28/2016.
  */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
